6257ae560e85e4dbfc879e06061da01db59f1c1c
IvanEscobar <ivanescobar.tfr@gmail.com>
2026-04-07 22:20:25 -0300

    primer commit

---
+++ corte_y_navaja/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for corte_y_navaja project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'corte_y_navaja.settings')
+
+application = get_asgi_application()
