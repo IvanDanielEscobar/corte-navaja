428a3dd4febd36cef45d0bffe8d80f57c19529ea
IvanEscobar <ivanescobar.tfr@gmail.com>
2026-05-05 22:14:24 -0300

    editar reserba

--- corte/views.py
+++ corte/views.py
@@ -45,3 +45,14 @@ def eliminar_reserva(request, id):
 def reserva_exitosa(request, id):
     reserva = get_object_or_404(Reserva, id=id)
     return render(request, 'corte/reserva.html', {'reserva':reserva})
+
+def editar_reserva(request, id):
+    reserva = get_object_or_404(Reserva, id=id)
+    if request.method == 'POST':
+        form = ReservaForm(request.POST, instance=reserva)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_reservas')
+    else:
+        form = ReservaForm(instance=reserva)
+    return render(request, 'corte/editar_reserva.html', {'form': form, 'reserva': reserva})
